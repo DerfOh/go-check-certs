e9dc98d7dafbafa8d948dc511516a5fb9c720af4
DerfOh <fredrick.p@outlook.com>
2017-03-27 00:26:32 -0400

    Add refresh function to  http server for live data

--- httpServer.go
+++ httpServer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func serveHTTP() {
@@ -14,9 +15,20 @@ func serveHTTP() {
 func handler(response http.ResponseWriter, request *http.Request) {
 	//response.Header().Set("Content-type", "text/html")
 	response.Header().Set("Content-type", "text/csv")
+	refreshResults()
 	webpage, err := ioutil.ReadFile("results.csv")
 	if err != nil {
 		http.Error(response, fmt.Sprintf("results.csv file error %v", err), 500)
 	}
 	fmt.Fprint(response, string(webpage))
+}
+
+// remove results file, then generate a new one
+func refreshResults(){
+	err := os.Remove("results.csv")
+	createOutPutFile()
+	processHosts()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
